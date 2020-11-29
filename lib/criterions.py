3be4e4eb5a651675cd3005fb5183cc8361216b0e
cmkwong <cheung.mankwong@yahoo.com.hk>
2020-11-29 22:32:51 +0800

    1. Subsampling the stories\n 2. added sub_sampling_from_sentcs_special() for 'is', 'the', 'to' special words\n 3. token_count calculate again after subsampling\n 4. Changed the embedding_size, number of noise vectors\n 5. Added Save network and load network function

---
+++ lib/criterions.py
@@ -0,0 +1,56 @@
+import torch
+from torch import nn
+import random
+import numpy as np
+
+class NegativeSamplingLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, input_vectors, output_vectors, noise_vectors):
+        batch_size, embed_size = input_vectors.shape
+
+        # Input vectors should be a batch of column vectors
+        input_vectors = input_vectors.view(batch_size, embed_size, 1)
+
+        # Output vectors should be a batch of row vectors
+        output_vectors = output_vectors.view(batch_size, 1, embed_size)
+
+        # bmm = batch matrix multiplication
+        # correct log-sigmoid loss
+        out_loss = torch.bmm(output_vectors, input_vectors).sigmoid().log()
+        out_loss = out_loss.squeeze()
+
+        # incorrect log-sigmoid loss
+        noise_loss = torch.bmm(noise_vectors.neg(), input_vectors).sigmoid().log()
+        noise_loss = noise_loss.squeeze().sum(1)  # sum the losses over the sample of noise vectors
+
+        # negate and sum correct and noisy log-sigmoid losses
+        # return average batch loss
+        return -(out_loss + noise_loss).mean()
+
+
+def cosine_similarity(embedding, valid_size=16, valid_window=100, device='cpu'):
+    """ Returns the cosine similarity of validation words with words in the embedding matrix.
+        Here, embedding should be a PyTorch embedding module.
+    """
+
+    # Here we're calculating the cosine similarity between some random words and
+    # our embedding vectors. With the similarities, we can look at what words are
+    # close to our random words.
+
+    # sim = (a . b) / |a||b|
+
+    embed_vectors = embedding.weight
+
+    # magnitude of embedding vectors, |b|
+    magnitudes = embed_vectors.pow(2).sum(dim=1).sqrt().unsqueeze(0)
+
+    # pick N words from our ranges (0,window) and (1000,1000+window). lower id implies more frequent
+    valid_examples = np.array(random.sample(range(valid_window), valid_size))
+    valid_examples = torch.tensor(valid_examples, dtype=torch.long).to(device)
+
+    valid_vectors = embedding(valid_examples)
+    similarities = torch.mm(valid_vectors, embed_vectors.t()) / magnitudes
+
+    return valid_examples, similarities
---
+++ main.py
@@ -0,0 +1,80 @@
+from lib import data, models, criterions
+import os
+import torch.optim as optim
+import torch
+
+DATA_PATH = "../tasks_1-20_v1-2/en"
+FILE_NAME = "qa1_single-supporting-fact_train.txt"
+SAVE_PATH = "../docs/embedding"
+NET_FILE = "checkpoint-Epoch-{}.data".format(1000)
+SAVE_EPOCH = 1000
+PRINT_EVERY = 500
+LOAD_NET = False
+
+BATCH_SIZE = 64
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+# preprocess the story from txt file
+token_stories, answers, reasons, token_count, int2word, word2int, word_stories = data.preprocess_story(DATA_PATH, FILE_NAME)
+
+# combined into one list
+words = [word for story in token_stories for sentc in story for word in sentc]
+
+# get the noise distribution in reversed order
+noise_dist = torch.from_numpy(data.get_noise_dist(token_count, reversed=True))
+
+# set up the model
+embedding_dim = 64
+model = models.SkipGramNeg(len(token_count), embedding_dim, noise_dist=noise_dist).to(DEVICE)
+
+if LOAD_NET:
+    print("Loading net params...")
+    with open(os.path.join(SAVE_PATH, NET_FILE), "rb") as f:
+        checkpoint = torch.load(f)
+    model.load_state_dict(checkpoint['state_dict'])
+    print("Successful!")
+
+# using the loss that we defined
+criterion = criterions.NegativeSamplingLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.00001)
+
+steps, epoch = 0, 0
+# train for some number of epochs
+while True:
+    for inputs, targets in data.get_batches(words, BATCH_SIZE, window_size=1):
+        steps += 1
+        inputs, targets = torch.tensor(inputs, dtype=torch.long), torch.tensor(targets, dtype=torch.long)
+        inputs, targets = inputs.to(DEVICE), targets.to(DEVICE)
+
+        # input, output, and noise vectors
+        input_vectors = model.forward_input(inputs)
+        output_vectors = model.forward_output(targets)
+        noise_vectors = model.forward_noise(inputs.shape[0], 2)
+
+        # negative sampling loss
+        loss = criterion(input_vectors, output_vectors, noise_vectors)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # save the embedding layer
+        if epoch % SAVE_EPOCH == 0 and epoch > 0:
+            checkpoint = {"state_dict": model.state_dict()}
+            with open(os.path.join(SAVE_PATH, "checkpoint-Epoch-{}.data".format(epoch)), "wb") as f:
+                torch.save(checkpoint, f)
+
+        # loss stats
+        if steps % PRINT_EVERY == 0:
+            print("Epoch: ", epoch)
+            print("Loss: ", loss.item())  # avg batch loss at this point in training
+            valid_examples, valid_similarities = criterions.cosine_similarity(model.in_embed, valid_size=16, valid_window=len(token_count), device=DEVICE)
+            _, closest_idxs = valid_similarities.topk(6)
+
+            valid_examples, closest_idxs = valid_examples.to('cpu'), closest_idxs.to('cpu')
+            for ii, valid_idx in enumerate(valid_examples):
+                closest_words = [int2word[idx.item()] for idx in closest_idxs[ii]][1:]
+                print(int2word[valid_idx.item()] + " | " + ', '.join(closest_words))
+            print("...\n")
+
+    epoch += 1
